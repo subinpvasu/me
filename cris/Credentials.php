2d03a6559dc629ce92d44aad3e4aec5dd0b805d2
subinpvasu <subinpvasu@gmail.com>
2020-10-29 23:09:19 +0530

    Credentials updated again

--- cris/Credentials.php
+++ cris/Credentials.php
@@ -7,8 +7,8 @@
  
 class Credentials{
     public static $MASTER_ID = '6076703147';    
-    public static $CLIENT_ID = '435665370423-lcvhar9ud41sei5jakivd7mo093b8aqs.apps.googleusercontent.com';
-    public static $CLIENT_SECRET = '7aLu_rdAd9sT1XODR-IgX3em';
+    public static $CLIENT_ID = '435665370423-h80snqui3lpjti2j1bo3jato5qrutitb.apps.googleusercontent.com';
+    public static $CLIENT_SECRET = 'fEAg7llOzq8f2myofd80icvd';
     public static $DEVELOPER_TOKEN = '164oteZ_x-t9PxaFy1Mikg';
     public static $REFRESH_TOKEN = '1//0gUofpRkkIC5-CgYIARAAGBASNwF-L9Irg4lPpR3-NAlXm5EFhE7JgbxJ3dqI9ROUPW0n3Cse2X-PJL2BrtHRYXytwkogxg49bEw';
     public static $STATUS_FIELD = 16;
